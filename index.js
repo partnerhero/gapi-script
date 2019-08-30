6cce8112246f43344395c8080c79f55ebb62bad1
Lucas Andrade Oliveira <lucasandrad@yandex.com>
2019-08-30 14:20:01 -0300

    Remove arrow function to make this package work with projects without babel #1

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { gapi, gapiComplete } from './gapiScript';
 
-const loadAuth2 = async (clientId, scopes) => {
+const loadAuth2 = async function (clientId, scopes) {
     return new Promise(resolve => {
         gapi.load('auth2', () => {
             resolve(gapi.auth2.init({
                 client_id: clientId,
                 scope: scopes
-            }))
-        })
-    })
+            }));
+        });
+    });
 }
 
-export { gapi, gapiComplete, loadAuth2 }
+export { gapi, gapiComplete, loadAuth2 };
