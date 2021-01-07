83cd7c5aa2a24a7e584cea9b50c49dbdcd9b5909
Lucas Andrade <lucas.oliveira@partnerhero.com>
2021-01-06 22:51:12 -0300

    Add a function loadClientAuth2 from https://github.com/partnerhero/gapi-script/issues/14#issue-717843305 @natalieh235

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 import { gapi, gapiComplete } from './gapiScript';
 
 const loadAuth2 = async function (clientId, scopes) {
-    return new Promise(resolve => {
-        gapi.load('auth2', () => {
-            resolve(gapi.auth2.init({
-                client_id: clientId,
-                scope: scopes
-            }));
-        });
+  return new Promise(resolve => {
+    gapi.load('auth2', () => {
+      resolve(gapi.auth2.init({
+        client_id: clientId,
+        scope: scopes
+      }));
     });
+  });
 }
 
 const loadAuth2WithProps = async function (props) {
-    return new Promise(resolve => {
-        gapi.load('auth2', () => {
-            resolve(gapi.auth2.init(props));
-        });
+  return new Promise(resolve => {
+    gapi.load('auth2', () => {
+      resolve(gapi.auth2.init(props));
     });
+  });
 }
 
-export { gapi, gapiComplete, loadAuth2, loadAuth2WithProps };
+
+const loadClientAuth2 = async function (clientId, scopes) {
+  return new Promise(resolve => {
+      gapi.load('client', () => {
+          resolve(gapi.client.init({
+              client_id: clientId,
+              scope: scopes
+          }));
+      });
+      gapi.load('auth2', () => {
+          resolve(gapi.client.init({
+              client_id: clientId,
+              scope: scopes
+          }));
+      });
+  });
+}
+
+export { gapi, gapiComplete, loadAuth2, loadAuth2WithProps, loadClientAuth2 };
